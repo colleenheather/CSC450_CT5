861aa6aed5d9f309da966eebfbc0a11842d66bb7
Colleen Ammons <colleen.ammons@gmail.com>
2023-11-12 11:39:18 -0500

    adding part (b) ct5.cpp
    added a method to reverse file contents and store in another file

--- CSC450_CT5/ct5.cpp
+++ CSC450_CT5/ct5.cpp
@@ -1,23 +1,44 @@
 //===========================================================================
 // Name        : ct5 (Critical Thinking 5)
 // Author      : Colleen Ammons
-// Description : write to a file without changing previous content
-// Pseudocode  :	obtain file(CSC450_CT5_mod5.txt)
-//					if cant open
-//                      output("unable to open file")
-//                      exit
-//                  else
-//						write("some text")
-//						file.close()
-//					    output("finished")
+// Description :(a) write to a file without changing previous content
+//              (b) reverse file contents and store in another file
+
+// Pseudocode  :	(a)
+//					    if cant open file
+//                          output("failed") & exit
+//                      else
+//						    writeToFile("some text")
+//                  (b)
+//                      if cant open file
+//                          output("failed") & exit)
+//                      else
+//                          string str
+//                          for char in file
+//                              str + char
+//                          reverse(str) =>
+//                              l = len of str -1, r = 0
+//                              while(r > l) { swap , l--, r++}
+//                          writeToFile(str)
 //============================================================================
 
 #include <iostream>
 #include <fstream>
+#include <string>
 
-int main() {
-    std::string filePath = "file_folder/CSC450_CT5_mod5.txt";
+void reverseString(std::string& s) {
+    int left = 0;
+    int right = s.length() - 1;
 
+    while (left < right) {
+        std::swap(s[left], s[right]); // or can use temp var
+        left++;
+        right--;
+    }
+}
+
+
+int append_to_file(std::string &filePath) {
     // Open the file in append mode
     std::ofstream file(filePath, std::ios::app);
 
@@ -29,5 +50,43 @@ int main() {
     file << "This is a new line - Colleen\n";
     file.close();
     std::cout << "Text appended to" << filePath << " successfully" << std::endl;
+    return 0;   
+}
+
+// recommended to use binary 
+int reverse_file(std::string &filePath, std::string& newFile){
+    std::ifstream ifs(filePath, std::ios::binary);
+    if (!ifs)
+        //import errorhandling?
+        //throw std::runtime_error("Can't open input file.");
+        std::cerr << "Error opening input file: " << std::endl;
+
+    std::string s;
+    for(char ch; ifs.get(ch);)
+       s.push_back(ch);
+
+    //OR 
+    // #include <algorithms>
+    //std::reverse(s.begin(), s.end());
+
+    reverseString(s);
+
+    std::ofstream ofs(newFile, std::ios::binary);
+    if (!ofs) throw std::runtime_error("Couldn't open output file.");
+
+    for(char ch: s)
+        ofs.put(ch);
+    
     return 0;
 }
+
+
+int main() {
+    std::string filePath = "file_folder/CSC450_CT5_mod5.txt";
+    std::string newFilePath = "file_folder/new_reversed_file.txt";
+
+    append_to_file(filePath);
+    reverse_file(filePath, newFilePath);
+    return 0;
+}
+
